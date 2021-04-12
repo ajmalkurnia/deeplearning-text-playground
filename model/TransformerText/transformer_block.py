f1ddb35ce840563289b214b2ef71a7272919ab38
Ajmal Kurnia <ajmalprayoga@gmail.com>
2021-04-12 20:48:05 +0700

    Rework multihead attention

--- model/TransformerText/transformer_block.py
+++ model/TransformerText/transformer_block.py
@@ -6,7 +6,7 @@ import keras
 import tensorflow as tf
 
 
-class MultiAttention(Layer):
+class MultiAttentionOld(Layer):
     def __init__(self, n_heads, dim_k, dim_v, **kwargs):
         """
         Initialize multi head attention layer
@@ -14,7 +14,7 @@ class MultiAttention(Layer):
         :param dim_k: int, length of query & key vector
         :param dim_v: int, length of value vector
         """
-        super(MultiAttention, self).__init__(**kwargs)
+        super(MultiAttentionOld, self).__init__(**kwargs)
         self.n_heads = n_heads
         self.dim_k = dim_k
         self.dim_v = dim_k
@@ -53,6 +53,67 @@ class MultiAttention(Layer):
         return output
 
 
+class MultiAttention(Layer):
+    def __init__(self, n_heads, dim_head, attention_do=0.5, **kwargs):
+        """
+        Initialize multi head attention layer
+        :param n_heads: int, number of attention heads
+        :param dim_head: int, length of query, key, and value for each head
+        """
+        super(MultiAttention, self).__init__(**kwargs)
+        self.n_heads = n_heads
+        self.dim_head = dim_head
+        self.attention_do = attention_do
+
+    def build(self, input_shape):
+        self.W_q = Dense(self.n_heads*self.dim_head, use_bias=False)  # Wq
+        self.W_k = Dense(self.n_heads*self.dim_head, use_bias=False)  # Wk
+        self.W_v = Dense(self.n_heads*self.dim_head, use_bias=False)  # Wv
+
+        # Get hidden/feature shape of the input
+        self.W_o = Dense(input_shape[0][2], use_bias=False)
+
+        self._dropout = Dropout(self.attention_do)
+
+    def reshape_heads(self, vec):
+        vec = K.reshape(
+            vec, (-1, vec.shape[1], self.n_heads, self.dim_head)
+        )
+        return K.permute_dimensions(vec, (0, 2, 1, 3))
+
+    def call(self, inputs, training):
+        if len(inputs) == 2:
+            query, key, value = inputs[0], inputs[1], inputs[1]
+        else:
+            query, key, value = inputs
+
+        q_vec = self.W_q(query)  # B x S x (n_heads * d )
+        k_vec = self.W_k(key)  # B x S x (n_heads * d )
+        v_vec = self.W_v(value)  # B x S x (n_heads * d )
+        # split heads and d
+        q_vec = self.reshape_heads(q_vec)  # B x n_heads x S x d
+        k_vec = self.reshape_heads(k_vec)  # B x n_heads x S x d
+        v_vec = self.reshape_heads(v_vec)  # B x n_heads x S x d
+
+        # Attention calculation
+        # B x n_heads x S x S
+        attention = tf.matmul(q_vec, k_vec, transpose_b=True)
+        scale = tf.sqrt(float(self.dim_head))
+        attention /= scale
+        # TODO: Apply mask here
+        attention = K.softmax(attention)
+        attention = self._dropout(attention, training=training)
+
+        # B x S x n_heads x d
+        o_seq = tf.einsum("bhij,bhjd->bihd", attention, v_vec)
+        # B x S x (n_heads * d)
+        o_seq = K.reshape(o_seq, (
+            -1, o_seq.shape[1], self.dim_head*self.n_heads
+        ))
+        output = self.W_o(o_seq)  # B x S x attention_d
+        return output
+
+
 class TransformerBlock(Layer):
     def __init__(self, dim_ff, dropout, n_heads, embed_dim, **kwargs):
         """
@@ -70,7 +131,7 @@ class TransformerBlock(Layer):
 
     def build(self, input_shape):
         self.mha = MultiAttention(
-            self.n_heads, self.att_dim, self.att_dim
+            self.n_heads, self.att_dim
         )
         self.ffn_hidden = Dense(self.dim_ff, activation="relu")
         self.ffn_out = Dense(self.att_dim)
